cd0dce2e9ea9f33b14a611efed27ca90abc4d595
Sil Westerveld <dev@silwesterveld.com>
2015-03-23 16:40:56 +0100

    Correct indenting and extend Movie and OMDbMovie class with new functions for shortened title and release date.

--- media.py
+++ media.py
@@ -1,18 +1,26 @@
 import json
 import re
 import urllib2
+import datetime
 
 
 class Movie():
-	'''A data structure to store my favorite movies'''
+    '''A data structure to store my favorite movies'''
 
-	VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
+    VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
 
-        def __init__(self, title, poster_image_url, trailer_youtube_url, imdb_url):
-		self.title = title
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
-		self.imdb_url = imdb_url
+    def __init__(self, title, poster_image_url, trailer_youtube_url, imdb_url):
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+        self.imdb_url = imdb_url
+
+    def shorten_title(self, words=4):
+        split_title = self.title.split()
+        if len(split_title) > words:
+            return unicode(' '.join(split_title[:words])) + ' &hellip;'
+        else:
+            return self.title
 
 
 class OMDbMovie(Movie):
@@ -33,23 +41,27 @@ class OMDbMovie(Movie):
         '''
         Initialize a Movie object with OMDb info retrieved from the OMDb API.
         '''
-        self.imdb_id = self.url_to_id(imdb_url)
+        self.imdb_id = self.get_imdb_id(imdb_url)
         self.imdb_url = imdb_url
-        self.trailer_youtube_url= youtube_trailer
         self.query_omdb(self.imdb_id)
-        Movie.__init__(self, self.omdb_json['Title'], self.omdb_json['Poster'], youtube_trailer, imdb_url)
 
-    def url_to_id(self, url):
+        Movie.__init__(self, self.omdb['Title'], self.omdb['Poster'], youtube_trailer, imdb_url)
+
+    def get_imdb_id(self, url):
         '''
         Extract IMDb ID from the URL provided
         For a URL like http://www.imdb.com/title/tt1375666/ it should return tt1375666
         '''
         pattern = re.compile(r'tt[0-9]{7}')
         match = re.search(pattern, url)
-        imdbID = match.group()
-        return imdbID
+        iid = match.group()
+        return iid
 
-    def query_omdb(self, imdbID):
-        '''Get OMDb info about movie'''
-        response = urllib2.urlopen(self.OMDB_BASEURL + imdbID)
-        self.omdb_json = json.load(response)
+    def get_release_date(self):
+        year, month, day = [int(e) for e in self.omdb['Released'].split('-')]
+        return datetime.date(year, month, day)
+
+    def query_omdb(self, iid):
+        '''Get OMDb info about movie, based on IMDb ID.'''
+        response = urllib2.urlopen(self.OMDB_BASEURL + iid)
+        self.omdb = json.load(response)
